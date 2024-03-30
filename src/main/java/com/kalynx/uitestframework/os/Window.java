95794986d63f3422eeeb8c70a8abe6b7711ae24f
raider001 <furnell.daniel@gmail.com>
2024-03-31 09:48:32 +1030

    Add javadoc

--- src/main/java/com/kalynx/uitestframework/os/Window.java
+++ src/main/java/com/kalynx/uitestframework/os/Window.java
@@ -17,7 +17,7 @@ public interface Window {
     /**
      * Gets the window dimensions for the specified window
      *
-     * @param windowName
+     * @param windowName The name of the window
      * @return Window dimensions, or Null if window not found.
      */
     Rectangle getWindowDimensions(String windowName) throws WindowException;
@@ -25,39 +25,39 @@ public interface Window {
     /**
      * Sets the window position relative to its current position
      *
-     * @param windowName
-     * @param x
-     * @param y
-     * @return
+     * @param windowName The name of the window
+     * @param x The horizontal position on the screen
+     * @param y The vertical position on the screen
+     * @return True if the window position was set successfully, False otherwise
      */
     boolean setWindowPosition(String windowName, int x, int y);
 
     /**
      * Sets the window position based on the display reference
      *
-     * @param windowName
-     * @param displayReference
-     * @param x
-     * @param y
-     * @return
+     * @param windowName The name of the window
+     * @param displayReference The reference of the display the window is on.
+     * @param x The horizontal position on the screen
+     * @param y The vertical position on the screen.
+     * @return True if the window position was set successfully, False otherwise
      */
     boolean setWindowPosition(String windowName, String displayReference, int x, int y);
 
     /**
      * Sets the window size
      *
-     * @param windowName
-     * @param width
-     * @param height
-     * @return
+     * @param windowName The name of the window
+     * @param width The new width for the window
+     * @param height The new height for the window
+     * @return True if the window size was set successfully, False otherwise
      */
     boolean setWindowSize(String windowName, int width, int height);
 
     /**
      * Bring the window to the front.
      *
-     * @param windowName
-     * @return
+     * @param windowName The name of the window
+     * @return True if the window was brought to the front, False otherwise
      */
     boolean bringToFront(String windowName);
 }
